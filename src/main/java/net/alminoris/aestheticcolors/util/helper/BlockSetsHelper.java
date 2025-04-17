f7e49926a08f8944cf0cfdaa4c975c97645650d1
AOA <aoa@d.co>
2025-04-17 17:28:19 +0300

    Initial Commit

--- src/main/java/net/alminoris/aestheticcolors/util/helper/BlockSetsHelper.java
+++ src/main/java/net/alminoris/aestheticcolors/util/helper/BlockSetsHelper.java
@@ -9,29 +9,4 @@ public class BlockSetsHelper
                     "purple", "magenta", "pink", "blue",
                     "cyan", "light_blue", "green", "lime"
             };
-
-    public static final String[] STONES =
-            {
-                    "tuff", "blackstone", "andesite", "diorite", "granite",  "deepslate", "basalt", "quartz", "blackstonebricks"
-            };
-
-    public static final String[] WOODS =
-            {
-                    "oak", "birch", "spruce", "jungle", "acacia", "dark_oak",
-                    "crimson", "warped", "mangrove", "cherry", "bamboo"
-            };
-
-    public static final String[] EXTRA_WOODS =
-            {
-                    "hazelnut", "hornbeam", "hawthorn", "quince", "plum", "mango", "fig", "viburnum", "white_mulberry", "wild_cherry",
-                    "bauhinia", "pine", "fir", "cedar", "olive", "tamarisk"
-            };
-
-    public static String[] getWoods()
-    {
-        String[] combinedWoods = new String[WOODS.length + EXTRA_WOODS.length];
-        System.arraycopy(WOODS, 0, combinedWoods, 0, WOODS.length);
-        System.arraycopy(EXTRA_WOODS, 0, combinedWoods, WOODS.length, EXTRA_WOODS.length);
-        return combinedWoods;
-    }
 }
